2642a62e0cd87c5728f9b17d5244210884ab105c
Brice Hartmann <brice@bricehartmann.com>
2025-12-20 23:30:42 -0600

    search support

--- app/Models/BusStop.php
+++ app/Models/BusStop.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use App\Enums\BusDirection;
 use App\Enums\BusStopPosition;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 use MatanYadaev\EloquentSpatial\Objects\Point;
 use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
 class BusStop extends Model
 {
     use HasSpatial;
+    use Searchable;
 
     protected $fillable = [
         'stop_identifier',
@@ -25,4 +27,12 @@ class BusStop extends Model
         'direction' => BusDirection::class,
         'position' => BusStopPosition::class,
     ];
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'direction' => $this->direction->value,
+        ];
+    }
 }
